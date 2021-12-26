0b695081cdf6480c587fb155272bfde25ff4a36e
Ekaterina Komratova <ekaterina.komratova@gmail.com>
2021-12-26 11:38:56 +0300

    Added tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,61 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService cashbackHackService = new CashbackHackService();
 
     @Test
-    void remain() {
+    public void shouldPurchaseAmountToTheBorder() {
+        int expected = 500;
+        int actual = cashbackHackService.remain(500);
+
+        assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldPurchaseAmountMin() {
+        int expected = 999;
+        int actual = cashbackHackService.remain(1);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldPurchaseAmountMax() {
+        int expected = 1;
+        int actual = cashbackHackService.remain(999);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldPurchaseAmountAboveTheBorder() {
+        int expected = 999;
+        int actual = cashbackHackService.remain(1001);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldPurchaseAmountNull() {
+        int expected = 1000;
+        int actual = cashbackHackService.remain(0);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldPurchaseAmountEqualToTheBorder() {
+        int expected = 0;
+        int actual = cashbackHackService.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+
+
 }
